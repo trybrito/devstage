66e6a57807549036c048cab2d477ba809a2fbcd5
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-20 20:01:13 -0300

    styles: input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/button';
 import { IconButton } from '@/components/icon-button';
-import { ArrowRight, Copy } from 'lucide-react';
+import { InputField, InputIcon, InputRoot } from '@/components/input';
+import { ArrowRight, Copy, Mail, User } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -12,6 +13,20 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <InputRoot>
+        <InputIcon>
+          <Mail className="size-5" />
+        </InputIcon>
+        <InputField type="email" placeholder="Digite seu e-mail" />
+      </InputRoot>
+
+      <InputRoot error>
+        <InputIcon>
+          <User className="size-5" />
+        </InputIcon>
+        <InputField type="email" placeholder="Digite seu usuário" />
+      </InputRoot>
     </main>
   );
 }
