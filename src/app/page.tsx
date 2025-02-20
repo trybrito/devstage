e47621752db13e898fe72e6e9eefe17a5f180488
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-20 18:31:17 -0300

    chore: tailwindcss | styles: global styles | styles: button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from '@/components/button';
+import { IconButton } from '@/components/icon-button';
+import { ArrowRight, Copy } from 'lucide-react';
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
+      <IconButton>
+        <Copy />
+      </IconButton>
     </main>
   );
 }
