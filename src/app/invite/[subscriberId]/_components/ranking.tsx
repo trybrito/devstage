2b35c6b78a2b5955dc1336de073321fb05ee8a58
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-21 01:06:01 -0300

    feat: stats and ranking

--- src/app/invite/[subscriberId]/_components/ranking.tsx
+++ src/app/invite/[subscriberId]/_components/ranking.tsx
@@ -1,57 +1,52 @@
-import Image from 'next/image';
+import Image, { type ImageProps } from 'next/image';
 
 import medalCopper from '../../../../assets/medal-copper.svg';
 import medalGold from '../../../../assets/medal-gold.svg';
 import medalSilver from '../../../../assets/medal-silver.svg';
+import { getRanking } from '@/http/api';
 
-export function Ranking() {
+const images: Record<string, ImageProps> = {
+  '1': medalGold,
+  '2': medalSilver,
+  '3': medalCopper,
+};
+
+export async function Ranking() {
+  const { ranking } = await getRanking();
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="font-heading font-semibold text-gray-200 leading-none text-xl">
         Ranking de indicações
       </h2>
 
-      <div className="space-y-4">
-        <div className="relative flex flex-col justify-center bg-gray-700 border border-gray-600 p-6 gap-3 rounded-xl">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1º</span> | Thiago Brito
-          </span>
+      {ranking.map((item, index) => {
+        const rankingPosition = index + 1;
+        const rankingImage = images[rankingPosition];
 
-          <span className="font-heading font-semibold text-2xl text-gray-200 leading-none">
-            1030
-          </span>
+        return (
+          <div key={item.id} className="space-y-4">
+            <div className="relative flex flex-col justify-center bg-gray-700 border border-gray-600 p-6 gap-3 rounded-xl">
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}º</span> |{' '}
+                {item.name}
+              </span>
 
-          <Image src={medalGold} alt="" className="absolute top-0 right-8" />
-        </div>
-      </div>
+              <span className="font-heading font-semibold text-2xl text-gray-200 leading-none">
+                {item.score}
+              </span>
 
-      <div className="space-y-4">
-        <div className="relative flex flex-col justify-center bg-gray-700 border border-gray-600 p-6 gap-3 rounded-xl">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2º</span> | Thiago Brito
-          </span>
-
-          <span className="font-heading font-semibold text-2xl text-gray-200 leading-none">
-            1030
-          </span>
-
-          <Image src={medalSilver} alt="" className="absolute top-0 right-8" />
-        </div>
-      </div>
-
-      <div className="space-y-4">
-        <div className="relative flex flex-col justify-center bg-gray-700 border border-gray-600 p-6 gap-3 rounded-xl">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3º</span> | Thiago Brito
-          </span>
-
-          <span className="font-heading font-semibold text-2xl text-gray-200 leading-none">
-            1030
-          </span>
-
-          <Image src={medalCopper} alt="" className="absolute top-0 right-8" />
-        </div>
-      </div>
+              <Image
+                src={rankingImage.src}
+                width={56}
+                height={86}
+                alt=""
+                className="absolute top-0 right-8"
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/app/invite/[subscriberId]/_components/stats.tsx
+++ src/app/invite/[subscriberId]/_components/stats.tsx
@@ -1,11 +1,26 @@
-import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
+import {
+  getSubscriberInviteClicks,
+  getSubscriberInviteCount,
+  getSubscriberRankingPosition,
+} from '@/http/api';
+import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react';
 
-export function Stats() {
+interface StatsProps {
+  subscriberId: string;
+}
+
+export async function Stats({ subscriberId }: StatsProps) {
+  const { count: accessCount } = await getSubscriberInviteClicks(subscriberId);
+  const { count: subscribesCount } = await getSubscriberInviteCount(
+    subscriberId
+  );
+  const { position } = await getSubscriberRankingPosition(subscriberId);
+
   return (
     <div className="grid gap-3 md:grid-cols-3">
       <div className="relative flex flex-col items-center justify-center bg-gray-700 border border-gray-600 px-4 py-7 gap-2 rounded-xl">
         <span className="font-heading text-2xl font-semibold leading-none">
-          1042
+          {accessCount}
         </span>
         <span className="text-sm leading-none text-gray-300 text-center">
           Acessos ao link
@@ -16,10 +31,10 @@ export function Stats() {
 
       <div className="relative flex flex-col items-center justify-center bg-gray-700 border border-gray-600 px-4 py-7 gap-2 rounded-xl">
         <span className="font-heading text-2xl font-semibold leading-none">
-          1042
+          {subscribesCount}
         </span>
         <span className="text-sm leading-none text-gray-300 text-center">
-          Acessos ao link
+          Inscrições feitas
         </span>
 
         <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
@@ -27,14 +42,14 @@ export function Stats() {
 
       <div className="relative flex flex-col items-center justify-center bg-gray-700 border border-gray-600 px-4 py-7 gap-2 rounded-xl">
         <span className="font-heading text-2xl font-semibold leading-none">
-          1042
+          {position ? `${position}ª` : 'N/A'}
         </span>
         <span className="text-sm leading-none text-gray-300 text-center">
-          Acessos ao link
+          Posição no ranking
         </span>
 
         <Medal className="size-5 text-purple absolute top-3 left-3" />
       </div>
     </div>
-  )
+  );
 }
